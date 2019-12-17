35696310c8ba0a33d15a13a41f68fee6a4cf27bc
EvvTim <evgeniytim94@gmail.com>
2019-12-17 02:09:09 +0100

    switch

---
+++ switch.js
@@ -0,0 +1,27 @@
+let day = 'wtorek';
+
+switch (day) {
+    case 'poniedziałek':
+        console.log('Dziś jest poniedziałek!');        
+        break;
+
+    case 'wtorek': 
+        console.log('Dziś jest wtorek!');        
+        break; 
+
+    case 'środa': 
+        console.log('Dziś jest środa!');        
+        break;
+
+    case 'czwartek': 
+        console.log('Dziś jest czwartek!');        
+        break;
+
+    case 'piątek': 
+        console.log('Dziś jest piątek!');        
+        break;
+
+    default:
+        console.log('Dziś jest weekend');
+        break;
+}
