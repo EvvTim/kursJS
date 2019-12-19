a6007a7e7916e2018a06e1c8e2dad25fbc0bac26
EvvTim <evgeniytim94@gmail.com>
2019-12-19 13:50:13 +0100

    functions

---
+++ Domyślne parametry funkcji.js
@@ -0,0 +1,8 @@
+const hello = (name = `drogi użytkowniku`) => {
+    return `Witaj ${name}, jak się masz?`;
+};
+
+console.log(hello('Żenia'));
+
+
+const add =(x,y) => x + y;
---
+++ Spread & rest operator v2.js
@@ -0,0 +1,17 @@
+// SPREAD 
+const arr = ['green', 'blue', 'yellow'];
+// console.log(...arr1, ...arr2);
+
+
+// REST
+
+function numbers(x,y,...z) {
+    console.log(x,y,z);
+}
+
+numbers(4,6,76,3,67,8,45,78.53)
+
+
+
+
+
