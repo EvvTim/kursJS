db668a28b602e5a61afcc479256bc364e3528a9b
EvvTim <evgeniytim94@gmail.com>
2019-12-28 00:25:34 +0100

    Projekt#2

--- kod.js
+++ kod.js
@@ -1,10 +1,21 @@
 const img1 = document.querySelector('.item1');
-const img2 = document.querySelector('.item2');
 const btn = document.querySelector('.arrow');
+const arrow = document.querySelector('.fas');
 
 
 const hideFun = () => {
     img1.classList.toggle('hide');
+
+    if (img1.classList.contains('hide')) {
+        
+        arrow.style.transform = 'rotate(180deg)';
+        
+    } else {
+        
+        arrow.style.transform = 'rotate(0deg)';
+
+    }
 }
 
 btn.addEventListener('click', hideFun);
+
