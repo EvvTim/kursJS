60f5a30004db5580fa62993cfbb74f510daeba95
EvvTim <evgeniytim94@gmail.com>
2019-12-30 20:23:25 +0100

    obiekty

---
+++ obiekty.js
@@ -0,0 +1,13 @@
+const newUser = {
+    name: 'Zhenia',
+    age: 25,
+
+    bike: {
+        brand: 'Dolan',
+        color: 'Grey',
+        year: 2018,
+    }
+}
+
+
+console.log(`${newUser.name} jezdzi rowerem marki ${newUser.bike.brand} :P`);
