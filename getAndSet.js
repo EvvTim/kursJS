93129e546c18601b741a5f281a3ad0ce2cab980b
EvvTim <evgeniytim94@gmail.com>
2020-01-14 12:42:08 +0100

    Get & set

---
+++ getAndSet.js
@@ -0,0 +1,25 @@
+const city = {
+    name: 'New York',
+    location: 'USA',
+
+
+    get cityName() {
+        return this.name;
+    },
+
+    set cityName(newCity) {
+
+        return this.name = newCity;
+
+    }
+}
+
+// console.log(city.name);
+
+console.log(city.cityName);
+
+const newCity = city.cityName = 'LA';
+
+console.log(newCity);
+
+console.log(city); 
