c87e2ba1c02a1dbf96219be693f331ef35cf4d02
EvvTim <evgeniytim94@gmail.com>
2020-01-16 11:22:37 +0100

    Fetch, than & catch

---
+++ FetchThanCatch/FetchThanCatch.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('.btn1');
+const img = document.querySelector('img');
+const url = 'https://dog.ceo/api/breeds/image/random';
+
+button.addEventListener('click', function() {
+    fetch(url)
+    .then(res => res.json())
+    .then(res => {
+        img.setAttribute('src', res.message);
+        img.style.width = '300px';
+})
+    .catch(err => console.log(err))
+
+
+})
+
+
+
+    
