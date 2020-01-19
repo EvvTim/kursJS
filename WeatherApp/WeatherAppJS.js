6bd3093bbff449e9652d9a16e5dd2e387dab544e
EvvTim <evgeniytim94@gmail.com>
2020-01-19 23:13:31 +0100

    Create function getWeather, add temp and hum

--- WeatherApp/WeatherAppJS.js
+++ WeatherApp/WeatherAppJS.js
@@ -14,3 +14,26 @@ const apiKey = '&APPID=41f33b51dd518167d4518ba7bf9b6d22'
 const units = '&units=metric';
 let city;
 let url;
+
+const getWeather = () => {
+
+    // city = input.value;
+    city = 'Warsaw';
+
+    url = apiLink + city + apiKey + units;
+    axios.get(url)
+        .then(res => {
+            const temp = res.data.main.temp;
+            const hum = res.data.main.humidity;
+
+            cityName.textContent = res.data.name;
+            temperature.textContent = `${Math.floor(temp)}°C`;
+            humidity.textContent = `${hum}%`;
+
+            console.log(`City: ${city}`);
+            console.log(`Temperature: ${temp}`);
+            console.log(`Humidity: ${hum}`);
+        })
+}
+
+getWeather();
