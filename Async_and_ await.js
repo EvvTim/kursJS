9eb2fdba6f26d19fad37bab3e23309cdb003a2e8
EvvTim <evgeniytim94@gmail.com>
2020-01-19 16:02:53 +0100

    Axios

--- Async_and_ await.js
+++ Async_and_ await.js
@@ -1,52 +1,55 @@
-// const test = new Promise((resolve,reject) => {
-//     const score = 1+3;
+function checkAge(age) {
 
-//     if (score === 2) {
-//         resolve('ok');
-//     } else {
-//         reject('edef');
-//     }
-// })
-
-// test
-//     .then(info => console.log(info))
-//     .catch(err => console.log(err))
-
-
-function checkAge (age) {
     return new Promise((resolve, reject) => {
+
         console.log(`Twój wiek to: ${age}`);
 
         if (age >= 18) {
-             resolve('Jest ok, możesz wejść');
+            resolve('Jest ok, możesz wejść')
         } else {
-            reject('Niestety jesteś za młody... ')
+            reject('Niestety jesteś za młody...')
         }
-    })
-}
 
+    })
+};
 
 function doubleCheck(ageConfirm) {
     return new Promise((resolve, reject) => {
-        console.log(`Sprawdze jeszcze raz.`);
+
+        console.log('Sprawdze jeszcze raz.');
         resolve(`Faktycznie: ${ageConfirm}`)
     })
 };
 
 
-checkAge(2)
-    .then(res => {
-        console.log(`.then RES: ${res}`);
-    })
+// checkAge(2)
+//     .then(res => {
+//         console.log(`.then RES: ${res}`);
+//         return doubleCheck(res);
+//     })
+//     .then(doubleCheck => {
+//         console.log(doubleCheck);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
 
 
 
+async function checkAge2() {
+
+  try {
+        const age = await checkAge(2)
+
+    console.log(age);
+
+    const dblCheck = await doubleCheck(age)
+
+    console.log(dblCheck);
+} catch(err) {
+    console.log(err);
+}
+  }
 
 
-
-
-
-
-                                  
-
-
+checkAge2()
