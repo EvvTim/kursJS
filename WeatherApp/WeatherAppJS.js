355d647656f10e0681ae9522ecd29d04a1ae049e
EvvTim <evgeniytim94@gmail.com>
2020-01-19 22:19:32 +0100

    WeatherApp - API

---
+++ WeatherApp/WeatherAppJS.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+
+const cityName = document.querySelector('.city-name');
+const warning = document.querySelector('.warning');
+const photo = document.querySelector('.photo');
+
+const weather = document.querySelector('.weather');
+const temperature = document.querySelector('.temp');
+const humidity = document.querySelector('.humidity');
+
+const apiLink = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '&APPID=41f33b51dd518167d4518ba7bf9b6d22'
+const units = '&units=metric';
+let city;
+let url;
